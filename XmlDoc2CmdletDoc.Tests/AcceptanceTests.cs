778af0c6d4ec434a82b5819b6a0aa7b267eaadbe
Chris Lambrou <github@chrislambrou.com>
2014-06-23 06:34:55 +0100

    More granular tests.

--- XmlDoc2CmdletDoc.Tests/AcceptanceTests.cs
+++ XmlDoc2CmdletDoc.Tests/AcceptanceTests.cs
@@ -1,7 +1,10 @@
 ﻿
 using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
 using NUnit.Framework;
-using XmlDoc2CmdletDoc.TestModule;
 using XmlDoc2CmdletDoc.TestModule.Manual;
 
 namespace XmlDoc2CmdletDoc.Tests
@@ -9,8 +12,29 @@ namespace XmlDoc2CmdletDoc.Tests
     [TestFixture]
     public class AcceptanceTests
     {
-        [Test]
-        public void Test()
+        private static readonly XNamespace mshNs = XNamespace.Get("http://msh");
+        private static readonly XNamespace mamlNs = XNamespace.Get("http://schemas.microsoft.com/maml/2004/10");
+        private static readonly XNamespace commandNs = XNamespace.Get("http://schemas.microsoft.com/maml/dev/command/2004/10");
+        private static readonly XNamespace devNs = XNamespace.Get("http://schemas.microsoft.com/maml/dev/2004/10");
+
+        private static readonly IXmlNamespaceResolver resolver;
+
+        static AcceptanceTests()
+        {
+            var manager = new XmlNamespaceManager(new NameTable());
+            manager.AddNamespace("", mshNs.NamespaceName);
+            manager.AddNamespace("maml", mamlNs.NamespaceName);
+            manager.AddNamespace("command", commandNs.NamespaceName);
+            manager.AddNamespace("dev", devNs.NamespaceName);
+            resolver = manager;
+        }
+
+        private XElement rootElement;
+        private XElement testManualElementsCommandElement;
+
+
+        [TestFixtureSetUp]
+        public void SetUp()
         {
             // ARRANGE
             var assemblyPath = typeof(TestManualElementsCommand).Assembly.Location;
@@ -21,14 +45,68 @@ namespace XmlDoc2CmdletDoc.Tests
             }
 
             // ACT
-            Program.Main(new[] {assemblyPath});
+            Program.Main(new[] { assemblyPath });
 
             // ASSERT
             Assert.That(File.Exists(cmdletXmlHelpPath));
-            var actualContent = File.ReadAllText(cmdletXmlHelpPath);
-            var expectedPath = Path.Combine(Path.GetDirectoryName(GetType().Assembly.Location), "expectedHelp.xml");
-            var expectedContent = File.ReadAllText(expectedPath);
-            Assert.That(actualContent, Is.EqualTo(expectedContent));
+
+            using (var stream = File.OpenRead(cmdletXmlHelpPath))
+            {
+                var document = XDocument.Load(stream);
+                rootElement = document.Root;
+            }
+            testManualElementsCommandElement = rootElement.XPathSelectElement("command:command[command:details/command:name/text() = 'Test-ManualElements']", resolver);
+        }
+
+        [Test]
+        public void ContainsACommandEntrryForTestManualElements()
+        {
+            Assert.That(testManualElementsCommandElement, Is.Not.Null);
+        }
+
+        [Test]
+        public void Manual_TestSynopsis()
+        {
+            Assume.That(testManualElementsCommandElement, Is.Not.Null);
+
+            var synopsisDescription = testManualElementsCommandElement.XPathSelectElement("command:details/maml:description", resolver);
+
+            Assert.That(synopsisDescription, Is.Not.Null);
+
+            var expectedXml =
+@"<description xmlns=""http://schemas.microsoft.com/maml/2004/10"">
+  <para>This is part of the Test-ManualElement synopsis.</para>
+  <para>This is also part of the Test-ManualElement synopsis.</para>
+</description>";
+            Assert.That(synopsisDescription.ToSimpleString(), Is.EqualTo(expectedXml));
+        }
+
+        [Test]
+        public void Manual_TestDescription()
+        {
+            Assume.That(testManualElementsCommandElement, Is.Not.Null);
+
+            var synopsisDescription = testManualElementsCommandElement.XPathSelectElement("maml:description", resolver);
+
+            Assert.That(synopsisDescription, Is.Not.Null);
+
+            var expectedXml =
+@"<description xmlns=""http://schemas.microsoft.com/maml/2004/10"">
+  <para>This is part of the Test-ManualElement description.</para>
+  <para>This is also part of the Test-ManualElement description.</para>
+</description>";
+            Assert.That(synopsisDescription.ToSimpleString(), Is.EqualTo(expectedXml));
+        }
+
+        [Test]
+        public void Manual_WhenThereAreNoParameterSets_ThereShouldBeOnlyOneCommandSyntaxItem()
+        {
+            Assume.That(testManualElementsCommandElement, Is.Not.Null);
+
+            var syntaxItems = testManualElementsCommandElement.XPathSelectElements("command:syntax/command:syntaxItem", resolver).ToList();
+
+            Assert.That(syntaxItems, Is.Not.Empty);
+            Assert.That(syntaxItems.Count, Is.EqualTo(1));
         }
     }
 }
---
+++ XmlDoc2CmdletDoc.Tests/XElementExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+
+namespace XmlDoc2CmdletDoc.Tests
+{
+    public static class XElementExtensions
+    {
+        public static string ToSimpleString(this XElement element)
+        {
+            return element.Simplify().ToString(SaveOptions.OmitDuplicateNamespaces);
+        }
+
+        private static XElement Simplify(this XElement element)
+        {
+            var newElement = new XElement(element.Name);
+            foreach (var attribute in element.Attributes())
+            {
+                newElement.Add(attribute);
+            }
+            foreach (var node in element.Nodes().Where(x => x is XElement || x is XText))
+            {
+                if (node is XElement)
+                {
+                    newElement.Add(((XElement) node).Simplify());
+                }
+                else
+                {
+                    newElement.Add(((XText) node).Value.Trim());
+                }
+            }
+            return newElement;
+        }
+    }
+}
