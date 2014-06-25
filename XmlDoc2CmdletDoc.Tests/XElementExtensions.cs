cbd45dd1dcaf63cb37ae6c5955b5b59ba1707a91
Chris Lambrou <github@chrislambrou.com>
2014-06-25 07:26:10 +0100

    Code tidy.

--- XmlDoc2CmdletDoc.Tests/XElementExtensions.cs
+++ XmlDoc2CmdletDoc.Tests/XElementExtensions.cs
@@ -5,15 +5,26 @@ namespace XmlDoc2CmdletDoc.Tests
 {
     public static class XElementExtensions
     {
+        /// <summary>
+        /// Returns a simple, clean string representation of the 
+        /// </summary>
+        /// <param name="element"></param>
+        /// <returns></returns>
         public static string ToSimpleString(this XElement element)
         {
+            // Strip out superfluous text and comments from the element.
             element = element.Simplify();
+
+            // Add the element to a container element with the common namespace prefixes defined,
+            // so that it adops those prefixes.
             var container = new XElement("container",
                 new XAttribute(XNamespace.Xmlns + "maml", "http://schemas.microsoft.com/maml/2004/10"),
                 new XAttribute(XNamespace.Xmlns + "command", "http://schemas.microsoft.com/maml/dev/command/2004/10"),
                 new XAttribute(XNamespace.Xmlns + "dev", "http://schemas.microsoft.com/maml/dev/2004/10"),
                 element);
             element = container.Elements().First();
+
+            // And then format it nicely.
             return element.ToString(SaveOptions.OmitDuplicateNamespaces);
         }
 
@@ -24,13 +35,13 @@ namespace XmlDoc2CmdletDoc.Tests
             {
                 newElement.Add(attribute);
             }
-            foreach (var node in element.Nodes().Where(x => x is XElement || x is XText))
+            foreach (var node in element.Nodes())
             {
                 if (node is XElement)
                 {
                     newElement.Add(((XElement) node).Simplify());
                 }
-                else
+                else if (node is XText)
                 {
                     newElement.Add(((XText) node).Value.Trim());
                 }
