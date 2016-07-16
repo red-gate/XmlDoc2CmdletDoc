d9c5f62f0f05d0a3ceb6d1077595208bd47cbe78
Chris Lambrou <github@chrislambrou.com>
2016-07-16 09:54:13 +0100

    Version bump

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -18,6 +18,6 @@ using System.Runtime.InteropServices;
 
 // The following attributes are automatically set by the build scripts. Please do not modify them manually.
 // Instead, change the value defined in version-number.txt.
-[assembly: AssemblyVersion("0.2.3")]
-[assembly: AssemblyFileVersion("0.2.3")]
-[assembly: AssemblyInformationalVersion("0.2.3")]
+[assembly: AssemblyVersion("0.2.4")]
+[assembly: AssemblyFileVersion("0.2.4")]
+[assembly: AssemblyInformationalVersion("0.2.4")]
